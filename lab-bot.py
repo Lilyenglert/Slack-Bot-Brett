17e5a78e0dcf06c18cc1c738ea909cb73b35bdf7
lmc327 <lmc327@cornell.edu>
2019-03-27 15:45:03 -0400

    forward bot working

--- lab-bot.py
+++ lab-bot.py
@@ -19,6 +19,25 @@ MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 def parse_event(slack_events):
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
+            print(event["channel"])
+            if event["channel"] == "DH51YLLJX" or "DH5F2SWLD":
+                print("in channel")
+                text = event["text"]
+                channel = "CH289F6SX"
+                if text != None:
+                    slack_client.api_call(
+                        "chat.postMessage",
+                        channel=channel,
+                        text=text
+                        )
+                    slack_client.api_call(
+                        "chat.update",
+                        channel=channel,
+                        ts=timestamp,
+                        text="dontknow"
+                        )
+                time.sleep(RTM_READ_DELAY)
+                return text, "CH289F6SX", event["ts"]
             return event["text"], event["channel"], event["ts"]
     return None, None, None
 
@@ -29,22 +48,23 @@ if __name__ == "__main__":
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
             text, channel, timestamp = parse_event(slack_client.rtm_read())
-            if text != None:
-                x = raw_input("Say something: ")
-                string = "{}".format(text, str(x))
-                print (string)
+            # print(channel)
+            # if text != None:
+            #     x = input("Say something: ")
+            #     string = "{}".format(text, str(x))
+                
                     
-                slack_client.api_call(
-                    "chat.postMessage",
-                    channel=channel,
-                    text=x
-                    )
-                slack_client.api_call(
-                    "chat.update",
-                    channel=channel,
-                    ts=timestamp,
-                    text="dontknow"
-                    )
-            time.sleep(RTM_READ_DELAY)
+            #     slack_client.api_call(
+            #         "chat.postMessage",
+            #         channel=channel,
+            #         text=x
+            #         )
+            #     slack_client.api_call(
+            #         "chat.update",
+            #         channel=channel,
+            #         ts=timestamp,
+            #         text="dontknow"
+            #         )
+            # time.sleep(RTM_READ_DELAY)
     else:
         print("Connection failed. Exception traceback printed above.")
