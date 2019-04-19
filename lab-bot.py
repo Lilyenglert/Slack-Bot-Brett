aa795da0c4fe81f906e878b13852bf8a8db0753d
lmc327 <lmc327@cornell.edu>
2019-04-19 16:30:59 -0400

    fixes and instructions put in document

--- lab-bot.py
+++ lab-bot.py
@@ -11,43 +11,53 @@ starterbot_id = None
 
 # DEFINE NEW MEMBERS FOR THE CHAT:  
 #### To find these ID's uncomment the section with the header "ID UPDATE FOR NEW PARTICIPANTS"
-# Participant user id:
 participant_user_id = "UH2MEGTU5"
-# Participant - bot channel id: 
-part_bot_channel_id = "DH5F2SWLD"
-# Group channel id: 
 group_channel_id = "CH289F6SX"
-# Researcher - bot channel id:
-researcher_bot_channel_id = "UH5CJN1D3"
+part_bot_channel_id = "DH5F2SWLD"
+researcher_bot_channel_id = "DH51YLLJX"
+researcher_user_id = "UH5CJN1D3"
 
 # "UH2MEGTU5" = Devki user id 
 # "DH5F2SWLD" for Devki-bot Channel
 # "DH51YLLJX" for Lily Channel
 # group channel ID = "CH289F6SX"
 
-
-target_channel = "DH5F2SWLD"
+target_channel = group_channel_id
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_event(target_channel, slack_events):
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
-            # ID UPDATE FOR NEW PARTICIPANTS: Uncomment the lines one at a time and send the message 
-            # participant ID , 
-            ############
-            print()
-            ############
+            ## ID UPDATE FOR NEW USERS: Uncomment, run, and recomment the lines between ######## one at a time and copy the code from the terminal:
+            
+            ## participant_user_id: and group_channel_id:
+            ##########
+            # print("participant_user_id: ", event["user"], " group_channel_id: ", event["channel"] )
+            ##########
+            ## save file and re-run lab bot. Write a message from the participant in the group channel
 
-            if event["user"] == "UH5CJN1D3":
+            ## part_bot_channel_id: 
+            ##########
+            # print("part_bot_channel_id: ", event["channel"])
+            ##########
+            ## save file and re-run lab-bot. Write a message from the participant in the personal message with the bot
+
+            ## researcher_user_id: and researcher_bot_channel_id:
+            ##########
+            # print("researcher_user_id: ", event["user"], "researcher_bot_channel_id: ", event["channel"])
+            ##########
+            ## save file and re-run lab-bot. Write a message from the participant in the personal message with the bot
+
+            if event["user"] == researcher_user_id:
                 # check whether the message text says GROUP or PARTICIPANT 
                 text = event["text"]
                 if text == 'GROUP':
                     # group channel ID = "CH289F6SX"
-                    target_channel = "CH289F6SX"  #group_target_channel
+                    target_channel = group_channel_id  #group_target_channel
                     print("You have switched to the GROUP channel. If you would like to change back please write PARTICIPANT")
                 elif text == 'PARTICIPANT':
-                    target_channel = "DH5F2SWLD" #participant_target_channel
+                    target_channel = part_bot_channel_id #participant_target_channel
                     print("You have switched to the PARTICIPANT channel. If you would like to change back please write GROUP") 
                 elif text == 'ERASE ALL MESSAGES':
                     #### ADD function to clear channel
@@ -61,12 +71,13 @@ def parse_event(target_channel, slack_events):
 
             # if the participant sends a personal message, then print that message in the researchers
             # chat with the prefix: participant says: 
-            elif event["user"] == "UH2MEGTU5" and event["channel"] == "DH5F2SWLD":
+            elif event["user"] == participant_user_id and event["channel"] == part_bot_channel_id:
                 text = event["text"]
                 if text != None:
                     text = "The Participant: " + event["text"]
-                    target_channel = "DH51YLLJX"
+                    target_channel = researcher_bot_channel_id
                     send_message(target_channel, text, timestamp)
+                    target_channel = part_bot_channel_id 
                 time.sleep(RTM_READ_DELAY)
                 return text, target_channel, event["ts"]
 
